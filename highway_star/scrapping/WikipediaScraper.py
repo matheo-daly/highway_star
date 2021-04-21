f5a1f8a897204563ae78ab6353e21ba3bd5c6e7a
matheo-daly <matheodaly.md@gmail.com>
2021-04-21 23:48:36 +0200

    fixup! rearrange-project-for-easier-use-with-package

--- highway_star/scrapping/WikipediaScraper.py
+++ highway_star/scrapping/WikipediaScraper.py
@@ -106,6 +106,7 @@ def get_content_from_all_pages(all_pages: dict, start_tag: str, end_tag: str) ->
     return all_pages
 
 
+
 def scrap_wikipedia_structure_with_content(root_category: str, start_tag: str, end_tag: str, lang: str):
     all_pages: dict = scrap_wikipedia_structure(subcategories=[root_category], lang=lang)
     all_pages_with_content: dict = get_content_from_all_pages(all_pages=all_pages, start_tag=start_tag, end_tag=end_tag)
