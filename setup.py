98b3f99d41b7e0cf0a21cbfbed192a0970ef1f71
matheo-daly <matheodaly.md@gmail.com>
2021-04-21 23:51:03 +0200

    change version of package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="highway_star",
-    version="0.0.3",
+    version="0.0.3.1",
     author="Mathéo Daly",
     author_email="matheodaly.md@gmail.com",
     description="A library to scrap content from wikipedia categories",
